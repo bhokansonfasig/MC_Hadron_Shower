50a9a175fb2c1e5fa2289a4b4ae46d4052729be8
Ben Hokanson-Fasig <bhokansonfasig@gmail.com>
2017-04-07 15:05:33 -0500

    Added (bad) interaction products

--- atmosphere.py
+++ atmosphere.py
@@ -6,12 +6,12 @@ def density(height):
     """Returns the number density (in m^-3) of the atmosphere at a given height"""
     return 0.02504e27
 
-def getAtmosphericNucleus():
+def getAtmosphericNucleus(position):
     """Returns an atomic nucleus based on the atmospheric composition"""
     atomSeed = random()
-    if atomSeed>1-.78:
-        return Particle("nitrogen")
-    elif atomSeed>.01:
-        return Particle("oxygen")
-    else:
-        return Particle("argon")
+    if atomSeed>=1-.78: #78% nitrogen
+        return Particle("nitrogen",pos=position)
+    elif atomSeed>=.01: #21% oxygen
+        return Particle("oxygen",pos=position)
+    else:              #1% argon
+        return Particle("argon",pos=position)
--- shower.py
+++ shower.py
@@ -2,6 +2,7 @@
 from random import random,choice
 from constants import pi
 from particle import Particle
+from atmosphere import density, getAtmosphericNucleus
 
 
 def randomValue(start,stop=None):
@@ -16,31 +17,99 @@ def generateRandomPrimary():
     # primaryTypes = ["proton"]
     particleType = "proton"
     position = [0,0,1000]
-    energy = 1000
+    energy = 10000
     theta = pi
     phi = 0
 
     return Particle(particleType,pos=position,E=energy,theta=theta,phi=phi)
 
 
-def getPropagationLength(particle):
-    """Return a propagation length for the particle"""
+def getNextInteraction(particle):
+    """Return a propagation length for the particle and the particle it
+    interacts with after the propagation (decay returns None as target)"""
     interactionLength = 100 #m
-    return interactionLength
+    if "mu" in particle.type or "pi" in particle.type:
+        target = None
+    else:
+        if particle.ke<140*3/2:
+            target = None
+        else:
+            target = getAtmosphericNucleus(particle.position)
+    return interactionLength, target
 
 def propagate(particle):
-    """Propagate the particle"""
-    distance = getPropagationLength(particle)
+    """Propagate the particle and return particle that caused it to stop
+    (decay returns None)"""
+    distance, target = getNextInteraction(particle)
     for i in range(len(particle.position)):
         particle.position[i] += distance * particle.direction[i]
+    return target
 
 
 def interact(particle,target=None):
     """Interact the particle with the target (or decay by default)
     and return the products"""
     if target is None:
-        return [particle]
+        if particle.type=="pi+":
+            return [Particle("mu+",pos=particle.position,energy=particle.energy,
+                             theta=particle.theta,phi=particle.phi),
+                    Particle("nuMu",pos=particle.position)]
+        elif particle.type=="pi-":
+            return [Particle("mu-",pos=particle.position,energy=particle.energy,
+                             theta=particle.theta,phi=particle.phi),
+                    Particle("nuMuBar",pos=particle.position)]
+        elif particle.type=="mu+":
+            return [Particle("e+",pos=particle.position,energy=particle.energy,
+                             theta=particle.theta,phi=particle.phi),
+                    Particle("nuE",pos=particle.position),
+                    Particle("nuMuBar",pos=particle.position)]
+        elif particle.type=="mu-":
+            return [Particle("e-",pos=particle.position,energy=particle.energy,
+                             theta=particle.theta,phi=particle.phi),
+                    Particle("nuEBar",pos=particle.position),
+                    Particle("nuMu",pos=particle.position)]
+        else:
+            print("Warning: particle",particle.type,"does not decay")
+            return [particle]
+    elif target.type=="N" or target.type=="O" or target.type=="Ar":
+        pionType = random()
+        if pionType>=2/3:   #1/3 pi+
+            splitEnergy = particle.energy/3
+            return [Particle(particle.type,pos=particle.position,energy=splitEnergy,
+                             theta=particle.theta,phi=particle.phi),
+                    Particle("n0",pos=particle.position,energy=splitEnergy,
+                             theta=particle.theta,phi=particle.phi),
+                    Particle("pi+",pos=particle.position,energy=splitEnergy,
+                             theta=particle.theta,phi=particle.phi)]
+        elif pionType>=1/3: #1/3 pi-
+            splitEnergy = particle.energy/3
+            return [Particle(particle.type,pos=particle.position,energy=splitEnergy,
+                             theta=particle.theta,phi=particle.phi),
+                    Particle("p+",pos=particle.position,energy=splitEnergy,
+                             theta=particle.theta,phi=particle.phi),
+                    Particle("pi-",pos=particle.position,energy=splitEnergy,
+                             theta=particle.theta,phi=particle.phi)]
+        else:               #1/3 pi0
+            splitEnergy = particle.energy/3
+            secondaryType = random()
+            if secondaryType>=1/2: #1/2 proton
+                return [Particle(particle.type,pos=particle.position,
+                                 energy=splitEnergy,theta=particle.theta,
+                                 phi=particle.phi),
+                        Particle("p+",pos=particle.position,energy=splitEnergy,
+                                 theta=particle.theta,phi=particle.phi),
+                        Particle("pi0",pos=particle.position,energy=splitEnergy,
+                                 theta=particle.theta,phi=particle.phi)]
+            else:                  #1/2 neutron
+                return [Particle(particle.type,pos=particle.position,
+                                 energy=splitEnergy,theta=particle.theta,
+                                 phi=particle.phi),
+                        Particle("n0",pos=particle.position,energy=splitEnergy,
+                                 theta=particle.theta,phi=particle.phi),
+                        Particle("pi0",pos=particle.position,energy=splitEnergy,
+                                 theta=particle.theta,phi=particle.phi)]
     else:
+        print("Warning: no interaction between",particle.type,"and",target.type)
         return [particle,target]
 
 
@@ -52,15 +121,11 @@ def generateShower():
     while not(finished):
         products = []
         for particle in particles:
-            if particle.type=="p+" or particle.type=="n0":
-                propagate(particle)
-                products.extend(interact(particle,Particle("nitrogen")))
-            elif particle.type=="pi+" or particle.type=="pi-":
-                propagate(particle)
-                products.extend(interact(particle,Particle("nitrogen")))
-            elif particle.type=="mu+" or particle.type=="mu-":
-                propagate(particle)
-                products.extend(interact(particle))
+            if particle.type=="p+" or particle.type=="n0" or \
+               particle.type=="pi+" or particle.type=="pi-" or \
+               particle.type=="mu+" or particle.type=="mu-":
+                target = propagate(particle)
+                products.extend(interact(particle,target))
             else:
                 products.append(particle)
         particles = products
