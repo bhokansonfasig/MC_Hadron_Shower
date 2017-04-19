Fixed inverse CDF calculation and added decays

atmosphere.py
@@ -5,7 +5,7 @@ from particle import Particle
 
 def density(height,scaleHeight=8000):
     """Returns the number density (in m^-3) of the atmosphere at a given height"""
-    return 0.02504e27*np.exp(-height/scaleHeight)
+    return 2.504e25*np.exp(-height/scaleHeight)
 
 def getAtmosphericNucleus(position):
     """Returns an atomic nucleus based on the atmospheric composition"""
@@ -18,7 +18,7 @@ def getAtmosphericNucleus(position):
         return Particle("argon",pos=position)
 
 
-def getInverseCDF(crossSection,particleHeight,particleTheta,scaleHeight=8000):
+def getCollisionInverseCDF(crossSection,particleHeight,particleTheta,scaleHeight=8000):
     """Returns a function that will give the distance value for a provided value
     of the cumulative density function"""
     def invCDF(val):
@@ -27,5 +27,26 @@ def getInverseCDF(crossSection,particleHeight,particleTheta,scaleHeight=8000):
         a = density(0)*crossSection
         b = scaleHeight/np.cos(particleTheta)
         c = np.exp(-particleHeight/scaleHeight)
-        return -b*np.log(1+ np.log(1-val)/(a*b*c))
+        if b>0:
+            n = 1-np.exp(-a*b*c)
+        else:
+            n = 1
+        print(a,b,c)
+        print("",particleHeight,particleTheta)
+        print(" ",val,a*b*c,1-np.exp(a*b*c))
+        print("  ",1-n*val,1+ np.log(1-n*val)/(a*b*c))
+        print("   ",-b*np.log(1+ np.log(1-n*val)/(a*b*c)))
+        return -b*np.log(1+ np.log(1-n*val)/(a*b*c))
     return invCDF
+
+
+def getAirCrossSection(energy):
+    """Returns the cross section of a particle with the air at given energy"""
+    s = 2*energy/1000*13 #GeV^2
+
+    sigmapp = 32.4 - 1.2*np.log(s) + 0.21*np.log(s)**2
+
+    sigmapair = sigmapp*4+100
+
+    #Convert from mb to m^2
+    return sigmapair*1e-31

interactions.py
@@ -1,6 +1,6 @@
 """Functions for handling particle collisions and decays"""
 import numpy as np
-from constants import pi
+from constants import pi, c
 from MCmethods import randomInRange, randomWithSum, isotropicAngles, randomMomentumTriangle
 from particle import Particle
 
@@ -156,3 +156,13 @@ def collision(particle,target):
 
     return products
 
+
+
+def getDecayInverseCDF(lifetime,beta):
+    """Returns a function that will give the distance value for a provided value
+    of the cumulative density function"""
+    def invCDF(val):
+        """Calculates the inverse cumulative density function for decay"""
+        d = beta*c*lifetime/np.sqrt(1-beta**2)
+        return -d*np.log(1-val)
+    return invCDF

randomTesting.py
@@ -1,7 +1,8 @@
 """File for testing distributions of randomly generated numbers"""
 import numpy as np
 import matplotlib.pyplot as plt
-from interactions import rotate3D
+from MCmethods import rotate3D, randomDistance
+from atmosphere import getCollisionInverseCDF
 
 
 def test0():
@@ -260,6 +261,34 @@ def test9():
         plt.show()
 
 
+def test10():
+    """Generate two random lengths and return shorter one"""
+    crossSection = 2e-25
+    z = 200000
+    theta = 2/3*np.pi
+
+    invCDF1 = getCollisionInverseCDF(crossSection,z,theta)
+    invCDF2 = getCollisionInverseCDF(3*crossSection,z,theta)
+
+    size = 10000
+
+    lengths = []
+    cdfs = []
+    for _ in range(size):
+        len1 = randomDistance(invCDF1)
+        len2 = randomDistance(invCDF2)
+        if len1<=len2:
+            lengths.append(len1)
+            cdfs.append(1)
+        else:
+            lengths.append(len2)
+            cdfs.append(2)
+
+    plt.hist(lengths,bins=50)
+    print(cdfs.count(1),cdfs.count(2))
+    plt.show()
+
+
 if __name__ == '__main__':
     # test0()
     # test1()
@@ -270,4 +299,5 @@ if __name__ == '__main__':
     # test6()
     # test7()
     # test8()
-    test9()
+    # test9()
+    test10()